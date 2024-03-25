ab03a347ca799e638a23ba976c434305848f721a
arthur <sebarth123@hotmail.com>
2024-03-25 02:02:11 +0100

    commit

---
+++ graph.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+p = 100000
+val = np.arange(-2, 2, 1 / p)
+fig, ax = plt.subplots()
+xdata, ydata = [0], [0]
+ln, = ax.plot([], [], 'ro', markersize=10)
+x_start = -1
+g = 1
+
+
+def init():
+    ax.set_xlim(-2.1, 2.1)
+    ax.set_ylim(-0.1, 1.1)
+    return ln,
+
+
+def f(x):
+    f_val = np.sin(x) ** 2 * (np.sign(x + 1) + 1) * -(np.sign(x - .8) - 1) - 2 * (np.sign(x + 1) - 1) * np.sin(
+        -1) ** 2 + 2 * np.sign(x - .8) * np.sin(.8) ** 2
+    return f_val / 2.8322936730942847 + 0.4332891417126131
+
+
+def f_prime(x):
+    return 2 * np.sin(x) * np.cos(x)
+
+
+def theta(x):
+    return (np.arctan(f_prime(x) - f_prime(x) * x / f(x) + f(x)))#  + np.pi/4 * (np.sign(x) + 1)) * (np.sign(x + 1) + 1) * (np.sign(x - .8) - 1) / 4
+
+
+def update(frame):
+    xdata[0] = frame
+    ydata[0] = f(frame)
+    ln.set_data(xdata, ydata)
+    return ln,
+
+
+def frames(start, stop, num) -> np.ndarray:
+    return_frames = np.array([start])
+    acceleration = np.array([0.])
+    speed = np.array([0.])
+    pos = np.array([start])
+    delta_t = .01
+    mu = 5
+    i = 0
+    while True:
+        x = pos[0]
+        acceleration = g * np.cos(theta(x)) * (np.cos(theta(x)) - mu * np.sin(theta(x)))
+        speed += acceleration * delta_t
+        pos += speed * delta_t
+        return_frames = np.append(return_frames, x)
+        i+=1
+        if i%10_000 == 0:
+            print(acceleration)
+            print(theta(x))
+        if (np.all(abs(acceleration) == 0) and i > 1_000) or i > num:
+            print(speed)
+            print(return_frames)
+            print(i)
+            break
+    return return_frames
+
+
+plt.plot(val, f(val))
+ani = FuncAnimation(fig, update, frames=frames(-.7, 1, 100_000),
+                    init_func=init, blit=True, interval=25/3, repeat_delay=1000)
+plt.show()
